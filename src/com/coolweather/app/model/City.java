85fea196ef8134da52a2075a0984513042c26f60
MarQnn <maqin@maqindeMacBook-Pro.local>
2018-04-22 00:10:21 +0800

    新增数据库帮助类，以及各表对应的实体类。

---
+++ src/com/coolweather/app/model/City.java
@@ -0,0 +1,44 @@
+package com.coolweather.app.model;
+
+public class City {
+	
+	private int id;
+	
+	private String cirName;
+	
+	private String cityCode;
+	
+	private int provinceId;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCirName() {
+		return cirName;
+	}
+
+	public void setCirName(String cirName) {
+		this.cirName = cirName;
+	}
+
+	public String getCityCode() {
+		return cityCode;
+	}
+
+	public void setCityCode(String cityCode) {
+		this.cityCode = cityCode;
+	}
+
+	public int getProvinceId() {
+		return provinceId;
+	}
+
+	public void setProvinceId(int provinceId) {
+		this.provinceId = provinceId;
+	}
+}
